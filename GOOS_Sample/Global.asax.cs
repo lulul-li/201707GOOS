cb034c764bb9000436e07f759a604e1b88f7900e
lulu <s987337613@gmail.com>
2017-07-02 22:39:12 +0800

    roll back all change

--- GOOS_Sample/Global.asax.cs
+++ GOOS_Sample/Global.asax.cs
@@ -13,7 +13,6 @@ namespace GOOS_Sample
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
-            UnityConfig.RegisterComponents();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
